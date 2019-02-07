ca1257c1d77582f595963f244fa30a8c3f962d30
rseroter <rseroter@hotmail.com>
2019-02-07 05:36:20 -0800

    Updated code

---
+++ MyService.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace framework_aspnet_service
+{
+    /// <summary>
+    /// Summary description for MyService
+    /// </summary>
+    [WebService(Namespace = "http://pivotal.io")]
+    // [System.Web.Script.Services.ScriptService]
+    public class MyService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string SayHi()
+        {
+            //get machine name
+            string machineName = Environment.MachineName;
+
+            //add system console writeline
+            System.Console.WriteLine(string.Format("Being polite and saying hi from {0}!", machineName));
+
+            //add read of environment variable
+            string greeting = Environment.GetEnvironmentVariable("greeting");
+
+            return string.Format("{0}, everyone!", greeting);
+        }
+
+        [WebMethod]
+        public void CrashMe()
+        {
+
+            System.Threading.Thread.CurrentThread.Abort();
+
+        }
+    }
+}
